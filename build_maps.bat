a7a2cf15bc539bd787d05909b20185ad08d27fd3
Sean Baggaley <sean@plussean.co.uk>
2016-02-01 18:58:56 +0000

    Fix map compiling

--- build_maps.bat
+++ build_maps.bat
@@ -2,6 +2,10 @@
 
 CALL "%CD%\utilities\variables.bat"
 
-PUSHD jm_maps
+PUSHD jm_maps\jm_maps\
 %SEVENZAEXE% a -tzip "%START%\jm_maps-dev.pk3" * -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak
+POPD
+
+PUSHD jm_maps\jm_maps2\
+%SEVENZAEXE% a -tzip "%START%\jm2_maps-dev.pk3" * -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak
 POPD
