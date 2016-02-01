b95594bf03079bdd1bd8216d281875944a9712a9
Sean Baggaley <sean@plussean.co.uk>
2016-02-01 20:44:56 +0000

    Fix previous

--- build_maps.bat
+++ build_maps.bat
@@ -8,8 +8,8 @@ DEL /F /Q "%OUTDIR%\jm2_maps-*.pk3"
 
 PUSHD jm_maps\
 cd jm_maps
-%SEVENZAEXE% a -tzip "%START%\jm_maps.pk3" * -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak
+%SEVENZAEXE% a -tzip "%OUTDIR%\jm_maps.pk3" * -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak
 cd ..\jm2_maps
-%SEVENZAEXE% a -tzip "%START%\jm2_maps-%JM2_MAPS_VERSION%.pk3" * -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak
+%SEVENZAEXE% a -tzip "%OUTDIR%\jm2_maps-%JM2_MAPS_VERSION%.pk3" * -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak
 cd ..
 POPD
