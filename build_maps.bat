907e2198c74dc2394add97ab0fd2559e516ae629
Sean Baggaley <sean@plussean.co.uk>
2016-02-01 19:10:18 +0000

    Actually fix map builder

--- build_maps.bat
+++ build_maps.bat
@@ -2,10 +2,10 @@
 
 CALL "%CD%\utilities\variables.bat"
 
-PUSHD jm_maps\jm_maps\
+PUSHD jm_maps\
+cd jm_maps
 %SEVENZAEXE% a -tzip "%START%\jm_maps-dev.pk3" * -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak
-POPD
-
-PUSHD jm_maps\jm_maps2\
+cd ..\jm2_maps
 %SEVENZAEXE% a -tzip "%START%\jm2_maps-dev.pk3" * -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak
+cd ..
 POPD
