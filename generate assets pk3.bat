a26069ff8b640475dce56372967a6696c53babbe
Hypnotoad90 <hypnospace@gmail.com>
2016-02-01 17:48:41 +0000

    Put wrong directories in bats, oops!

--- generate assets pk3.bat
+++ generate assets pk3.bat
@@ -1,3 +1,3 @@
 @echo off
-7z a -tzip ./jm_assets-dev.pk3 ./jm_assets/* -x!*.dbs -x!*.bak -x!*.backup1 -x!*.backup2 -x!*.backup3
+7z a -tzip ./jm_assets-dev.pk3 ./jm_assets/pk3/* -x!*.dbs -x!*.bak -x!*.backup1 -x!*.backup2 -x!*.backup3
 pause 
