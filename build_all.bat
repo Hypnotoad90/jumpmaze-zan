cfb3362e1a1a8d6bbc7a70d016c6f3ad7c2af294
Sean Baggaley <sean@plussean.co.uk>
2016-02-02 20:36:14 +0000

    Pass arguments to each build_*.bat file when using build_all.bat

--- build_all.bat
+++ build_all.bat
@@ -4,6 +4,6 @@
 CALL "%CD%\utilities\variables.bat" %*
 CALL "%CD%\utilities\mkvers.bat" %*
 
-CALL "%CD%\build_core.bat"
-CALL "%CD%\build_assets.bat"
-CALL "%CD%\build_maps.bat"
+CALL "%CD%\build_core.bat" %*
+CALL "%CD%\build_assets.bat" %*
+CALL "%CD%\build_maps.bat" %*
