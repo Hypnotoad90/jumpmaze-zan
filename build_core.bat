5bd642143af589ee1e364a4f2d6ccefedb4f70fc
Hypnotoad90 <hypnospace@gmail.com>
2016-04-26 23:09:46 +0100

    jm_core - bunch of stuff
    -Add 'admin mode'
    -Add hints library
    -Ensure cheaters can't set times
    -Update config

--- build_core.bat
+++ build_core.bat
@@ -31,5 +31,7 @@ acc "scripts/personal_teleport.acs" "acs/ptport.o"
 acc "scripts/jpxmapinfo.acs" "acs/JPXINFO.o"
 acc "scripts/svmz/game.acs" "acs/srvlmaze.o"
 acc "scripts/INVCLEAR.acs" "acs/invclear.o"
+acc "scripts/admin.acs" "acs/admin.o"
+acc "scripts/hints.acs" "acs/hints.o"
 %SEVENZAEXE% a -tzip "%OUTDIR%\jm_core%FILEVERSION%.pk3" * -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak
 POPD
